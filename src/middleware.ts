14d5767f110fa878d932f9896c7061a630e07b09
Webprojon <tokhirjonkhasanov@gmail.com>
2025-01-29 11:26:04 +0100

    fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
-export default function middleware(req: NextRequest) {
-	return withAuth(req);
-}
+export default function middleware(req: NextRequest) {}
 
 export const config = {
 	matcher: ["/create-post"],
